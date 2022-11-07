a03d225613691c2e90d16de08e9b417eacacc125
Camila Mata <camirmata@gmail.com>
2022-11-07 17:33:23 -0500

    Initial Commit

---
+++ HumansApp/HumansAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HumansAppApp.swift
+//  HumansApp
+//
+//  Created by Cami Mata on 11/7/22.
+//
+
+import SwiftUI
+
+@main
+struct HumansAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
